6e9905e750e7f7d6d93155c9e395865115c56eff
JairoPNT <jairo@pnt.com>
2026-02-07 20:44:43 -0500

    Fix: Backend allow ALL origins for debugging

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,8 @@ const allowedOrigins = [
 
 app.use(cors({
     origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+        // PERMISSIVE CORS FOR DEBUGGING
+        callback(null, true);
     },
     credentials: true
 }));
